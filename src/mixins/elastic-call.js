83e936aa6c5a7393e927e222ec46686de373802d
evanzummeren <erikvanzummeren@gmail.com>
2020-09-28 14:16:58 -0400

    add credentials

--- src/mixins/elastic-call.js
+++ src/mixins/elastic-call.js
@@ -1,10 +1,20 @@
 const axios = require('axios');
+import serverCredentials from './server.json';
 
 export default {
   methods: {
     apiCall() {
-      console.log(axios);
-      console.log('blabla mixin call')
+      console.log(serverCredentials)
+      axios.post('/user', {
+        firstName: 'Fred',
+        lastName: 'Flintstone'
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
     }
   }
 }
