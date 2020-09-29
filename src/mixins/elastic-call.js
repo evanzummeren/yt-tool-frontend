c134ac36c5a48fc411ff37da764fff7fb4c1607d
evanzummeren <erikvanzummeren@gmail.com>
2020-09-28 21:07:33 -0400

    add call

--- src/mixins/elastic-call.js
+++ src/mixins/elastic-call.js
@@ -3,14 +3,48 @@ import serverCredentials from './server.json';
 
 export default {
   methods: {
-    apiCall() {
+    // dit verplaatsen naar main search
+    generalCall(str) {
       console.log(serverCredentials)
-      axios.post('/user', {
-        firstName: 'Fred',
-        lastName: 'Flintstone'
+      axios.post(serverCredentials.url, {
+        "size": 20,
+        "from": 0,
+        "query": {
+          "bool": {
+            "must": [
+              {
+                "match": {
+                  "line": `${str}`
+                }
+              },
+              {
+                "has_parent": {
+                  "parent_type": "video",
+                  "query": {
+                    "bool": {
+                      "must": [
+                        {
+                          "match": {
+                            "online": false
+                          }
+                        },
+                        {
+                          "match": {
+                            "category": "altright"
+                          }
+                        }
+                      ]
+                    }
+                  }
+                }
+              }
+            ]
+          }
+        }
       })
       .then(function (response) {
-        console.log(response);
+        console.log(this);
+        return response;
       })
       .catch(function (error) {
         console.log(error);
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="home">
     <input 
-      v-model="message" 
+      v-model="keyword" 
       v-on:keyup.enter="performSearch"
       placeholder="search">
+
+      <ul>
+        <li v-for="result in results" v-bind:key="result">
+          {{ result }}
+        </li>
+      </ul>
 
   </div>
 </template>
 
 <script>
-import ElasticCall from '../mixins/elastic-call.js';
+// import ElasticCall from '../mixins/elastic-call.js';
+const axios = require('axios');
+import serverCredentials from '../mixins/server.json';
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
 
@@ -18,23 +26,77 @@ export default {
   // components: {
   //   HelloWorld
   // },
-  mixins: [ElasticCall],
   data: function() {
     return {
-      message: ""
+      keyword: "",
+      results: []
     }
   },
   beforeCreate: function() {
-    console.log(this.$route.params);
   },
   mounted: function() {
-    this.apiCall();
+    this.processCall(this.$route.params.query);
   },
   methods: {
     performSearch() {
-      console.log(this.message)
-      this.$router.push(`../../../../search/q/${this.message}/cat/blabla`)
+      // console.log(this.message)
+      this.$router.push(`../../../../search/q/${this.keyword}/cat/blabla`)
+    },
+    processCall(str) {
+      // var resp = this.generalCall(this.$route.params.query);
+      // console.log(resp);
 
+      var _this = this;
+
+      axios.post(serverCredentials.url, {
+        "size": 20,
+        "from": 0,
+        "query": {
+          "bool": {
+            "must": [
+              {
+                "match": {
+                  "line": `${str}`
+                }
+              },
+              {
+                "has_parent": {
+                  "parent_type": "video",
+                  "query": {
+                    "bool": {
+                      "must": [
+                        {
+                          "match": {
+                            "online": false
+                          }
+                        },
+                        {
+                          "match": {
+                            "category": "altright"
+                          }
+                        }
+                      ]
+                    }
+                  }
+                }
+              }
+            ]
+          }
+        }
+      })
+      .then(function (response) {
+        console.log('bla')
+        response.data.hits.hits.forEach(el => {
+          console.log(el)
+          _this.results.push(el._source.line)
+        });
+
+        // _this.results.push('a')
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    
     }
   }
 }
